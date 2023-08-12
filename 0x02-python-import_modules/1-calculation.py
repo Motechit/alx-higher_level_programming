651243247aa684a52ec6a198c24982aff477fb6e
Motechit <otikomosunmola@gmail.com>
2023-08-11 19:02:22 -0700

    Corrections to Python_import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """This is to print the additn, subtractn, multiple and quotient of 10 and 5"""
+    """This is to give the additn, subtractn, multiple and quotient of 10 and 5"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
