8a0d4770b3e1d8f38259cbc0d2b19c36d905f7a4
Motechit <otikomosunmola@gmail.com>
2023-09-11 05:03:39 -0700

    tasks all done

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines Rectangle module"""
+"""Defines Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class body"""
+    """class body."""
 
     def __init__(self, size):
 
