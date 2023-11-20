bb84dc5860d1a74d7e9b66a0395dc6632a6f606e
Motechit <otikomosunmola@gmail.com>
2023-11-20 05:14:03 +0100

    object mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,13 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(host="localhost", port=3306, user="ubuntu",
-                                         passwd="ubuntu", db=argv[3], charset="utf8")
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
                             id ASC".format(argv[4]))
-        rows = cursor.fetchall()
-        for row in rows:
-            if row[1] == argv[4]:
-                print(row)
-        cursor.close()
-        db.close()
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,13 +8,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(host="localhost", port=3306, user="ubuntu",
-                                         passwd="ubuntu", db=argv[3], charset="utf8")
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                                            (argv[4],))
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-        db.close()
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
